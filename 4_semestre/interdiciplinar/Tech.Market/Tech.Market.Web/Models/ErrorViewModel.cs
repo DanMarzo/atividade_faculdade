6e09f78b5100faf8c1236ef6bcaa0209131193f0
DanMarzo <marzogildan@gmail.com>
2025-10-30 22:55:28 -0300

    main projeto web para mobiles

---
+++ 4_semestre/interdiciplinar/Tech.Market/Tech.Market.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tech.Market.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
